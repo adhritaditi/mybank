2cdc10ea65c6184cf9ff906da977da90fec3f81c
adhritaditi <aditi.mahajan5@gmail.com>
2024-08-27 12:11:16 +0530

    main_bank.py

--- main_bank.py
+++ main_bank.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 openai = OpenAI(
-    api_key = os.getenv('echo "# mybank" >> README.md')
+    api_key = os.getenv('OPENAI_API_SECRET_KEY')
 )
 
 app = FastAPI()
